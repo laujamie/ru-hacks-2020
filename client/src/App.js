65a399d73b20720f6e2d63dc179a639f28c5f7c7
jessie-wu22 <55641601+jessie-wu22@users.noreply.github.com>
2020-05-16 19:22:00 -0400

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import { Container, Button, Row, Col } from "react-bootstrap";
+import Header from "./components/Header";
+import NotesCard from "./components/NotesCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="wrapper">
+        <div className="main">
+          <div className="container">
+            <div className="row">
+              <div className="col-x-5 title-container">
+                <Header />
+              </div>
+              <div className="col-xs-7 notes-container">
+                <NotesCard />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => (
+  <div>
+    <img src="logo.png" class="logo-main" alt="logo" />
+    <p className="title-container__subtitle">
+      Collective thinking made simpler.
+    </p>
+  </div>
+);
+
+export default Header;
