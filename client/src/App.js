bcb36ad6b3f5b1478305b8de50bfca3c47c327ad
jessie-wu22 <55641601+jessie-wu22@users.noreply.github.com>
2020-05-17 01:55:23 -0400

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,19 @@
-import React, { useReducer } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import { Container, Button, Row, Col } from 'react-bootstrap';
-import Header from './components/Header';
-import NotesCard from './components/NotesCard';
-import { reducer, initialState } from './stores/store';
+import Header from "./components/Header";
+import NotesCard from "./components/NotesCard";
 
 function App() {
-  const [store, dispatch] = useReducer(reducer, initialState);
   return (
-    <div>
-      <div className="wrapper">
-        <div className="main">
-          <div className="container">
-            <div className="row">
-              <div className="col-x-5 title-container">
-                <Header />
-              </div>
-              <div className="col-xs-7 notes-container">
-                <NotesCard store={store} dispatch={dispatch} />
-              </div>
-            </div>
-          </div>
+    <div className="row">
+      <div className="title-container">
+        <Header />
+      </div>
+      <div className="notes-container">
+        <div className="buttons-container">
+          <NotesCard />
         </div>
       </div>
     </div>
